363037c70762d003262c90e5d92ae5b933070ca1
Ryan Shin <rshin8763@gmail.com>
2018-05-01 12:40:27 -0700

    add omp pragmas

--- p2/p2script.sh
+++ p2/p2script.sh
@@ -3,15 +3,18 @@
 echo "RESULTS" > results.out
 echo "=======" >> results.out
 
-for t in 1 2 4 16
+for t in 1 2 4 8
 do
-    for n in 16 32 64 128 256 512 1024 2048
+    for c in 0 1
     do
-        echo NUMT=$t
-        echo NUMNODES=$n
-        /usr/local/common/gcc-7.3.0/bin/g++ -DNUMT=$t -DNUMNODES=$n p1.cpp -o p1 -lm -fopenmp
-        ./p1 >> results.out
+        for s in 0 1
+        do
+            echo COARSE=$c
+            echo STATIC=$c
+            /usr/local/common/gcc-7.3.0/bin/g++ -DCOARSE=$c -DSTATIC=$s p2.cpp -o p2 -lm -fopenmp
+            ./p2 >> results.out
+        done
     done
-
+    #newline
     echo >> results.out
 done
